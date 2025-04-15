a331539a25919d80b39444ed6069c8feff138891
Tayyab <muhammad.tayyab@khired.com>
2025-04-15 18:12:11 +0500

    change qty

--- frontend/src/components/Checkout.jsx
+++ frontend/src/components/Checkout.jsx
@@ -84,7 +84,7 @@ export const Checkout = () => {
                                             <span>$10</span>
                                             <button className="btn btn-size ms-1">S</button>
                                         </div>
-                                        <div className='ps-5'> x2</div>
+                                        <div className='ps-5'> x3</div>
                                     </div>
                                 </td>
                             </tr>
